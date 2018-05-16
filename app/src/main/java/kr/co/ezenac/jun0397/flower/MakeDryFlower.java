2c7e15413c5978fefc694b14bcad62b2d6b4d705
KYJ <kwhyj@naver.com>
2018-05-16 15:13:48 +0900

    //makeflower

--- app/src/main/java/kr/co/ezenac/jun0397/flower/MakeDryFlower.java
+++ app/src/main/java/kr/co/ezenac/jun0397/flower/MakeDryFlower.java
@@ -1,12 +1,14 @@
 package kr.co.ezenac.jun0397.flower;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import kr.co.ezenac.jun0397.flower.Bean.Flower;
 import kr.co.ezenac.jun0397.flower.adapter.FlowerListAdapter;
 
@@ -22,32 +25,48 @@ import kr.co.ezenac.jun0397.flower.adapter.FlowerListAdapter;
  */
 
 public class MakeDryFlower extends Fragment{
-    @BindView(R.id.flower_list) GridView flower_list;
+    @BindView(R.id.form_btn)
+    Button form_btn;
+    @BindView(R.id.line_btn)
+    Button line_btn;
+    @BindView(R.id.fill_btn)
+    Button fill_btn;
+    @BindView(R.id.mass_btn)
+    Button mass_btn;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.make_dry_flower, container, false);
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.make_flower_main, container, false);
         Activity parentActivity = getActivity();
         ButterKnife.bind(this,rootview);
 
-        ArrayList<Flower> flower = new ArrayList<>();
-
-        Flower flower1 = new Flower(0,0,"장미드라이",25000,"www.naver.com");
-        Flower flower2 = new Flower(0,0,"수국드라이",35000,"www.naver.com");
-        Flower flower3 = new Flower(0,0,"튤립드라이",65000,"www.naver.com");
-        Flower flower4 = new Flower(0,0,"오드드라이",75000,"www.naver.com");
-        Flower flower5 = new Flower(0,0,"해바라기이",15000,"www.naver.com");
-        flower.add(flower1);
-        flower.add(flower2);
-        flower.add(flower3);
-        flower.add(flower4);
-        flower.add(flower5);
-
-        FlowerListAdapter FlowerListadapter = new FlowerListAdapter(flower);
-        flower_list.setAdapter(FlowerListadapter);
 
         //ImageView viewz = rootview.findViewById(R.id.viewz);
         return rootview;
+    }
+    @OnClick(R.id.form_btn)
+    public void clickForm(View view) {
+        Intent intent = new Intent(getActivity(),Flower_select.class);
+        startActivityForResult(intent, 0);
+
+    }
+    @OnClick(R.id.mass_btn)
+    public void clickMass(View view) {
+        Intent intent = new Intent(getActivity(),Flower_select.class);
+        startActivityForResult(intent, 1);
+
+    }
+    @OnClick(R.id.line_btn)
+    public void clickLine(View view) {
+        Intent intent = new Intent(getActivity(),Flower_select.class);
+        startActivityForResult(intent, 2);
+
+    }
+    @OnClick(R.id.fill_btn)
+    public void clickFill(View view) {
+        Intent intent = new Intent(getActivity(),Flower_select.class);
+        startActivityForResult(intent, 3);
+
     }
 
 }
